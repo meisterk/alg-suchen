1ab249f1f989715fc584c6a325da37a374ef0c83
meisterk <franz.kohnle@sabel.com>
2020-10-08 15:46:27 +0200

    Implement method

--- lineare-suche.py
+++ lineare-suche.py
@@ -10,7 +10,10 @@ import unittest
 
 
 def lineare_suche(liste, wert):
-    pass
+    for i in range(0, len(liste) - 1):
+        if liste[i] == wert:
+            return i
+    return -1
 
 # Testcases
 
@@ -18,9 +21,45 @@ def lineare_suche(liste, wert):
 class TestLineareSuche(unittest.TestCase):
     def test_1(self):
         # Arrange
-        liste = [1,2,3,4,5,6,7,8]
+        liste = [1, 2, 3, 4, 5, 6, 7, 8]
         wert = 4
         expected = 3
+
+        # Act
+        result = lineare_suche(liste, wert)
+
+        # Assert
+        self.assertEqual(result, expected)
+
+    def test_2(self):
+        # Arrange
+        liste = [1, 2, 3, 4, 5, 6, 7, 8]
+        wert = 9
+        expected = -1
+
+        # Act
+        result = lineare_suche(liste, wert)
+
+        # Assert
+        self.assertEqual(result, expected)
+
+    def test_3(self):
+        # Arrange
+        liste = [2, 3, 1, 5, 8, 6, 7, 4]
+        wert = 1
+        expected = 2
+
+        # Act
+        result = lineare_suche(liste, wert)
+
+        # Assert
+        self.assertEqual(result, expected)
+
+    def test_4(self):
+        # Arrange
+        liste = []
+        wert = 1
+        expected = -1
 
         # Act
         result = lineare_suche(liste, wert)
